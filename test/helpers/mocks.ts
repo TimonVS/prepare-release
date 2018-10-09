6f1d496979ab20135f4129d1fdb20a77353874a1
Timon van Spronsen <timonvanspronsen@outlook.com>
2018-10-09 09:59:50 +0200

    Fix paginate mock to include done cb

--- test/helpers/mocks.ts
+++ test/helpers/mocks.ts
@@ -1,3 +1,9 @@
-export async function paginate(fn: any, cb: any) {
-  return fn.then(cb)
+import { AnyResponse } from '@octokit/rest'
+
+export async function paginate(
+  res: Promise<AnyResponse>,
+  callback: (response: any, done?: () => void) => void
+) {
+  const done = () => {}
+  return res.then(x => callback(x, done))
 }
